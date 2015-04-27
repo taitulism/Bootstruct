0d6fa829eaacb6d993eecb1e13e3577ad4bdc015
barak igal <baraki@wix.com>
2015-04-28 00:47:05 +0300

    Cleanup
    I don't think that you need the IIFE

--- index.js
+++ index.js
@@ -4,19 +4,12 @@ var Ctrl      = require('./lib/constructors/ctrl');
 var IO        = require('./lib/constructors/io');
 var f2j       = require('./lib/utils/f2j');
 
-// --------------------------------
-module.exports = (function () {
-	var folderMap = f2j(resolve('app'));
-	var ctrlObj   = parseCtrl(folderMap);
+//setup
+var folderMap = f2j(resolve('app'));
+var ctrlObj   = parseCtrl(folderMap);
+var RC        = new Ctrl(ctrlObj, 'RC');
 
-	//  Root Ctrl
-	var RC = new Ctrl(ctrlObj, 'RC');
 
-	var bts = function (req, res) {
-		var io = new IO(req, res);
-
-		io = RC.checkIn(io);
-	};
-
-	return bts;
-})();
+module.exports = function bootstruct(req, res) {
+	return RC.checkIn(new IO(req, res));
+};
