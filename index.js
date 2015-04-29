8e629041550b303ee5c95a5c6707c28882446e3c
taitulism <almonimi@server.fake>
2015-04-30 01:21:09 +0300

    barak - slim

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-var resolve   = require('path').resolve;
-var parseCtrl = require('./lib/parseCtrl');
-var Ctrl      = require('./lib/constructors/ctrl');
-var IO        = require('./lib/constructors/io');
-var f2j       = require('./lib/utils/f2j');
-
-//setup
-var folderMap = f2j(resolve('app'));
-var ctrlObj   = parseCtrl(folderMap);
-var RC        = new Ctrl(ctrlObj, 'RC');
-
-
-module.exports = function bootstruct(req, res) {
-	return RC.checkIn(new IO(req, res));
-};
+module.exports = require('./lib/constructors/ctrl');
