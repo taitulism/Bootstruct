ccdd2f6fdd20eb3e5e095aac9a94924daad6a2da
taitulism <almonimi@server.fake>
2015-05-02 23:48:23 +0300

    added basic tests

--- tests/app/foo/verbs/post.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (io) {
-    console.log(__filename);
-    io.res.write(__filename.match(/app.*/)[0] + '>');
-    io.next();
-};
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,19 +1,52 @@
+var forIn   = require('../lib/utils/forin');
 var request = require('request');
-var http = require('http');
-var bts = require('../');
-var app = bts.start('./tests/app');
+var http    = require('http');
+var sep     = require('path').sep;
+var bts     = require('../');
+var app     = bts.start('./tests/app');
 
-http.createServer(app).listen(1001, '127.0.0.1');
+/* 
+ * key   = url to test
+ * value = the expected string, as array
+ */
+var testsObj = {
+	'/'        : ['app/first.js', 'app/all.js', 'app/get.js', 'app/last.js'],
+	'/foo'     : ['app/first.js', 'app/foo/first.js', 'app/foo/verbs/all.js', 'app/foo/verbs/get.js', 'app/foo/last.js', 'app/last.js'],
+	'/foo/bar' : ['app/first.js', 'app/foo/first.js', 'app/foo/bar/first.js', 'app/foo/bar/all.js', 'app/foo/bar/get.js', 'app/foo/bar/last.js', 'app/foo/last.js', 'app/last.js'],
+	'/baz'     : ['app/first.js', 'app/baz.js', 'app/last.js']
+};
 
-describe('bootstruct', function() {
-	it("should respond with Bootstruct's flow", function(done) {
-		request("http://localhost:1001/", function(error, response, body) {
 
-			var expected = 'app\\first.js>app\\all.js>app\\get.js>app\\last.js>';
-
-			expect(body).toEqual(expected);
-			done();
-		});
+// resolve array to string and normalize OS path seperator
+function resolveExpected (url, flowArry) {
+	var resolved = flowArry.map(function (item,i) {
+		return item.replace(/\//g, sep);
 	});
-});
 
+	// returned example: "app\first.js>app\all.js>app\get.js>app\last.js"
+	return resolved.join('>');
+}
+
+describe('Bootstruct', function() {
+
+	forIn(testsObj, function (url, flowArry) {
+		var expectedStr = resolveExpected(url, flowArry);
+		
+		it('expecting: ' + expectedStr, function(done) {
+			// start server
+			var server = http.createServer(app).listen(1001, '127.0.0.1');
+
+			// request
+			request('http://localhost:1001' + url, function(error, response, body) {
+				expect(body).toEqual(expectedStr);
+
+				// stop server
+				server.close();
+
+				done();
+			});
+		});
+		
+	});
+
+});
