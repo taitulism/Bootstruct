cfc2281d2d86e2a0b25b203254d33f6acce43c8d
taitulism <almonimi@server.fake>
2015-05-20 12:28:13 +0300

    ctrl simplified

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-module.exports = require('./lib/constructors/ctrl');
+'use strict';
+
+var Ctrl    = require('./lib/constructors/ctrl');
+var IO      = require('./lib/constructors/io');
+var map     = require('./lib/utils/f2j');
+var resolve = require('path').resolve;
+
+
+
+
+ /*─────────────────────────────────────────────────────────────────────────────
+ │  Bootstruct outer API (exported)
+ │  --------------------
+ │
+ │  args:
+ │  	appRoot [str] - The folder name to be parse as the app's root-controller
+ │  
+ │  returns:
+ │  	middleware [fn] - To be called with: request, response.
+ │  
+ │	code stroy:
+ │		* Bootstruct Fn is called with a folder name and resolves it. default: 'app'.
+ │		* Next Bts maps this folder (if exists).
+ │		* create the root controller (RC):
+ │			new Ctrl(folderMap, id, owner);
+ │		* returns the final middleware function 
+ │
+*/
+function bootstruct (appRoot) {
+	var resolvedPath, rootMap, RC;
+
+	appRoot = appRoot || 'app';
+	
+	resolvedPath = resolve(appRoot);
+
+	rootMap = map(resolvedPath);
+
+	RC = new Ctrl(rootMap, 'RC', null);
+	
+	return function middleware (req, res) {
+		var io = new IO(req, res);
+
+		return RC.checkIn(io);
+	};
+}
+
+
+// -------------------------
+module.exports = bootstruct;
