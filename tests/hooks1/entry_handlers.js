edc3426c5549ab1c5335b988753ed907eb265877
Itai Tenenbaum <taitulism@gmail.com>
2016-11-17 21:58:22 +0200

    WIP

--- tests/hooks1/entry_handlers.js
+++ tests/hooks1/entry_handlers.js
@@ -1,9 +1,7 @@
-module.exports = function(io){
-
+module.exports = function (io) {
 	if (typeof this.public == 'string') {
 		io.res.write(this.public);
 	}
 
 	io.res.end();
-
 };
--- tests/hooks2/index.js
+++ tests/hooks2/index.js
@@ -1,26 +1,20 @@
 module.exports = function (io) {
-
 	var body = '';
-
 
 	// io_init
 	body +=  (io.initiated) ? io.initiated + '-' : 'IO_INIT_FAIL-';
-
 
 	// ctrl_proto
 	body +=  (this.qwe) ? this.qwe() + '-' : 'CTRL_PROTO_FAIL_1-';
 	body +=  (this.asd) ? this.asd() + '-' : 'CTRL_PROTO_FAIL_2-';
 
-
 	// io_proto
 	body +=  (io.qwe) ? io.qwe() + '-' : 'IO_PROTO_FAIL_1-';
 	body +=  (io.asd) ? io.asd() + '-' : 'IO_PROTO_FAIL_2-';
 
-
 	// entry_handlers
 	body +=  (this.public) ? this.public + '-' : 'ENTRY_HANDLER_FAIL_1';
 	body +=  (this.views)  ? this.views  + '-' : 'ENTRY_HANDLER_FAIL_2';
-
 
 	body += (this.global.stuff) ? this.global.stuff.item : 'DEFAULT_HANDLER_FAIL';
 
