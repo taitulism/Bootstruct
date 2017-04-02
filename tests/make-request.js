8f2073decaaa975288734d71b89d81dd2bbdc4f7
Taitu Lizenbaum <taitulism@gmail.com>
2017-04-02 09:42:02 +0300

    split specs

---
+++ tests/make-request.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const expect  = require('chai').expect;
+
+module.exports = makeRequest;
+
+function makeRequest (url, expectRes, done, server, verb) {
+	verb = verb || 'get';
+
+	request[verb]('http://localhost:8181' + url, function(err, response, body) {
+		/* Deal Breaker */ if (err) { requestErr(err); return; }
+
+		expect(body).to.eql(expectRes);
+
+		done && done();
+	});
+}
+
+function requestErr (err) {
+	console.log('request-module ERROR:\n',err);
+	return;
+}
