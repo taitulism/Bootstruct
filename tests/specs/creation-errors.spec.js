43b8710460879db00d3a52933e0d9ec1f54d965a
Itai Tenenbaum <taitulism@gmail.com>
2018-12-01 17:31:35 +0200

    Test app hook folder with no "index.js" file

--- tests/specs/creation-errors.spec.js
+++ tests/specs/creation-errors.spec.js
@@ -3,11 +3,19 @@ const expect  = require('chai').expect;
 const bts  = require('../../');
 
 describe('Creation errors', function () {
-    it('should throw an exception when web-root-folder path doesn\'t exist', function () {
+    it('throws an exception when web-root-folder path doesn\'t exist', function () {
         function failedApp () {
-            bts('../not-exist');
+            bts('./not-exist');
         }
 
-        expect(failedApp).to.throw('couldn\'t find');
+        expect(failedApp).to.throw('find the web-root folder');
+    });
+
+    it('throws an exception when an app hook folder doesn\'t contain an "index.js" file', function () {
+        function failedApp () {
+            bts('./tests/app-folders/creation-errors/app-hook-folder-no-index/www');
+        }
+
+        expect(failedApp).to.throw('Expecting an "index.js" file ');
     });
 });
