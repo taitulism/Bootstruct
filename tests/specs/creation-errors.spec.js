86a42978cfb26964460327782a3d175e4dfa2184
Itai Tenenbaum <taitulism@gmail.com>
2018-12-03 14:07:38 +0200

    shorten tests description

--- tests/specs/creation-errors.spec.js
+++ tests/specs/creation-errors.spec.js
@@ -3,7 +3,7 @@ const expect  = require('chai').expect;
 const bts  = require('../../');
 
 describe('Creation errors', function () {
-    it('throws an exception when web-root-folder path doesn\'t exist', function () {
+    it('throws when web-root-folder path doesn\'t exist', function () {
         function failedApp () {
             bts('./not-exist');
         }
@@ -11,7 +11,7 @@ describe('Creation errors', function () {
         expect(failedApp).to.throw('find the web-root folder');
     });
 
-    it('throws an exception when an app hook folder doesn\'t contain an "index.js" file', function () {
+    it('throws when an app hook folder doesn\'t contain an "index.js" file', function () {
         function failedApp () {
             bts('./tests/app-folders/creation-errors/app-hook-folder-no-index/www');
         }
@@ -19,7 +19,7 @@ describe('Creation errors', function () {
         expect(failedApp).to.throw('Expecting an "index.js" file');
     });
 
-    it('throws an exception when an `ignore` hook is not a string', function () {
+    it('throws when an `ignore` hook is not a string', function () {
         function failedApp () {
             bts('./tests/app-folders/creation-errors/ignore-not-string/www');
         }
@@ -27,7 +27,7 @@ describe('Creation errors', function () {
         expect(failedApp).to.throw('a string or an array of strings');
     });
 
-    it('throws an exception when an `ignore` hook item is not a string', function () {
+    it('throws when an `ignore` hook item is not a string', function () {
         function failedApp () {
             bts('./tests/app-folders/creation-errors/ignore-item-not-string/www');
         }
@@ -35,7 +35,7 @@ describe('Creation errors', function () {
         expect(failedApp).to.throw('a string or an array of strings');
     });
 
-    it('throws an exception when a file and a folder share the same name (in the web-root folder)', function () {
+    it('throws when a file and a folder share the same name (in the web-root folder)', function () {
         function failedApp () {
             bts('./tests/app-folders/creation-errors/file-folder-same-name/www');
         }
@@ -43,7 +43,7 @@ describe('Creation errors', function () {
         expect(failedApp).to.throw('a controller and a method with the same name');
     });
 
-    it('throws an exception when a controller method has no params', function () {
+    it('throws when a controller method has no params', function () {
         function failedApp () {
             bts('./tests/app-folders/creation-errors/method-no-params/www');
         }
@@ -51,7 +51,7 @@ describe('Creation errors', function () {
         expect(failedApp).to.throw('must handle at least one param');
     });
 
-    it('throws an exception when an `io_init` hook in use and has no params', function () {
+    it('throws when an `io_init` hook in use and has no params', function () {
         function failedApp () {
             bts('./tests/app-folders/creation-errors/no-params-io-init/www');
         }
@@ -59,7 +59,7 @@ describe('Creation errors', function () {
         expect(failedApp).to.throw('"io.init" function must handle an argument');
     });
 
-    it('throws an exception when an `io_init` hook is not a function', function () {
+    it('throws when an `io_init` hook is not a function', function () {
         function failedApp () {
             bts('./tests/app-folders/creation-errors/io-init-must-be-function/www');
         }
@@ -67,7 +67,7 @@ describe('Creation errors', function () {
         expect(failedApp).to.throw('"io_init" expected to be a function');
     });
 
-    it('throws an exception when an `io_exit` hook is not a function', function () {
+    it('throws when an `io_exit` hook is not a function', function () {
         function failedApp () {
             bts('./tests/app-folders/creation-errors/io-exit-must-be-function/www');
         }
@@ -75,7 +75,7 @@ describe('Creation errors', function () {
         expect(failedApp).to.throw('"io_exit" expected to be a function');
     });
 
-    it('throws an exception when `shared_ctrl` hook is not a folder', function () {
+    it('throws when `shared_ctrl` hook is not a folder', function () {
         function failedApp () {
             bts('./tests/app-folders/creation-errors/shared-ctrl-not-folder/www');
         }
