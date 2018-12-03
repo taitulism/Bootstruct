8afa29cf6c100c64421bee72c81c48753dbc12e8
Itai Tenenbaum <taitulism@gmail.com>
2018-12-03 14:18:33 +0200

    fix playground & debug tests

--- playground/play.js
+++ playground/play.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
+'use strict';
 
 const http = require('http');
 
 const bts = require('../');
-const app = bts('www');
+const app = bts(__dirname + '/www');
 
 http.createServer(app).listen(8080, () => {
     console.log('Listening on port 8080');
